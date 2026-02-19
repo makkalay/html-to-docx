91ef7e242ee39781914006584441bfd30c9871ce
Michael Pasa <makkallayy@gmail.com>
2026-02-19 18:35:25 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   telemetry: false,
+  nitro: {
+    serverAssets: [{
+      baseName: 'templates',
+      dir: './server/templates'  // './templates'
+    }],
+  },
 })
